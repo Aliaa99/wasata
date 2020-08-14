001df3a4a99c3c8283c940f5bf8aaef40d326b1c
Aliaa99 <aliaaahmed129@gmail.com>
2020-08-15 01:54:32 +0200

    merge

---
+++ who-we-are.php
@@ -0,0 +1,77 @@
+<?php
+    include 'controllers/header.php';
+?>
+    <div class="container m-h-50 mb-60 mt-30">
+
+        <!-- idea -->
+        <div class="idea">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="idea-photo">
+                        <img src="images/photo_header.png" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <article>
+                        <h2 class="line-style">عن وساطة</h2>
+                        <p>تأسست عام 1993 وهذا النص يمكن تغييره هذا النص يمكن تغييره هذاا النص يمكن تغييره هذا النص يمكن </p>
+                    </article>
+                    <article>
+                        <span><i class="fa fa-eye"></i></span>
+                        <h2>رؤيتنا المستقبلية</h2>
+                        <p>تأسست عام 1993 وهذا النص يمكن تغييره هذا النص يمكن تغييره هذاا النص يمكن تغييره هذا النص يمكن </p>                        
+                    </article>
+                    <article>
+                        <span><i class="fas fa-hand-paper"></i></span>
+                        <h2>الأهداف</h2>
+                        <p>تأسست عام 1993 وهذا النص يمكن تغييره هذا النص يمكن تغييره هذاا النص يمكن تغييره هذا النص يمكن </p>                        
+                    </article>
+                </div>
+            </div>
+            <div class="row">
+                <!-- experience -->
+                <div class="col-sm-6">
+                    <div class="experiece">
+                        <article>
+                            <h2>خبرات عن وساطة فى العمل </h2>
+                            <ul>
+                                <li> هذا النص يتغير هذا النص يتغير هذا النص يتغير هذا النص يتغير هذا  النص يتغير هذا النص يتغير النص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغيرالنص يتغير هذا النص يتغير</li>
+                                <li> هذا النص يتغير هذا النص يتغير هذا النص يتغير هذا النص يتغير هذا النص يتغير هذا النص يتغير</li>
+                            </ul>
+                        </article>                    
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="video-img">
+                        <a href="#myvideo"  data-toggle="modal" data-target="#myvideo">
+                            <img src="images/photo.png" alt="">
+                        </a>
+                    </div>
+                                    <!-- Modal -->
+                            <div class="modal fade text-center" id="myvideo" role="dialog">
+                                <div class="modal-dialog">
+                                
+                                    <!-- Modal content-->
+                                    <div class="modal-content p-0">
+                                        <div class="modal-body">
+                                            <div class="video-frame embed-responsive embed-responsive-16by9">
+                                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                
+                                </div>
+                            </div>
+
+
+                </div>            
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php
+    include 'controllers/footer.php';
+?>
