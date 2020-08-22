562afd0a7274f6d027d0f6d5d0744247b2412784
Aliaa99 <aliaaahmed129@gmail.com>
2020-08-22 07:55:28 +0200

    merge

---
+++ mycv.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <!-- Tab Title -->
+        <title>Rakan</title>
+        <!-- Shortcut Icon -->
+        <link rel="shortcut icon" href="images/logoicon.png" />
+        <!-- Font Awesome Css -->
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <!-- Bootstrap Css -->
+        <link rel="stylesheet" href="css/bootstrap-e.min.css" />
+        <link rel="stylesheet" href="fonts/font/ar/stylesheet.css" />
+        <link rel="stylesheet" href="fonts/icons/css/fontello.css" />
+        <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+
+        <link rel="stylesheet" href="css/bootstrap-a.min.css" />
+        <!-- Owl carousel -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css" type="text/css">
+        <!-- Lightbox -->
+        <link rel="stylesheet" href="css/lightbox.css" />
+        <!-- Styling File -->
+        <link rel="stylesheet" href="css/main.css" />
+
+
+
+
+
+<div class="mycv">
+    <div class="container">
+        <div class="cv-name">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-6">
+                    <h1>محمد أحمد على</h1>
+                    <p>نوع الإعاقة:<span>إعاقة جسدية</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="cv-details">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="personal-information">
+                    <div class="img-div">
+                        <img src="images/photo_cv.png" alt="">
+                    </div>
+                    <ul class="basic-info">
+                        <li>
+                            <p>محد أحمد على</p>
+                            <span><i class="fa fa-user"></i></span>
+                        </li>
+                        <li>
+                            <p>السعودية ,الدمام</p>
+                            <span><i class="fa fa-map-marker"></i></span>
+                        </li>
+                        <li>
+                            <p>12578565 / 54584126847</p>
+                            <span><i class="fa fa-phone"></i></span>
+                        </li>
+                        <li>
+                            <p>ahmed-gamal@gmail.com</p>
+                            <span><i class="fa fa-envelope"></i></span>
+                        </li>
+                    </ul>
+                    <ul class="more-info">
+                        <li>
+                            الميلاد : 
+                            <span>11 شعبان</span>
+                        </li>
+                        <li>
+                            الميلاد : 
+                            <span>11 شعبان</span>
+                        </li>
+                        <li>
+                            الميلاد : 
+                            <span>11 شعبان</span>
+                        </li>
+                        <li>
+                            الميلاد : 
+                            <span>11 شعبان</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+        <!-- Jquery js -->
+        <script src="js/jquery-3.2.1.js"></script>
+        <!-- Bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="js/slick.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
